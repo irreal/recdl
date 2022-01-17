9b5ae37ae1fb2b437af2d68a8a11747b0bdec44f
Miloš Spasojević <milos.s.pfc@gmail.com>
2022-01-17 10:55:53 +0100

    a bit of responsiveness

--- components/letter.tsx
+++ components/letter.tsx
@@ -13,7 +13,9 @@ function Letter({ letter, status }: { letter: string, status: CharacterMatch | n
         additionalClasses += "bg-yellow-500 text-white";
     }
     return (
-        <span className={`p-5 my-5 mx-1 border-2 border-black text-4xl font-bold w-20 h-20 inline-block text-center ${additionalClasses} `}>{letter.toUpperCase()}</span>
+        <div className={`grid my-5 mx-1 border-2 border-black w-10 h-10 md:w-20 md:h-20 ${additionalClasses} content-center justify-center`}>
+            <span className={`text-sm md:text-3xl font-bold`}>{letter.toUpperCase()}</span>
+        </div>
     )
 }
 
